1a2085f21525443cad58158e3c1b72c49a1e1785
dspham <dsvpham@gmail.com>
2018-12-12 01:27:44 +0000

    updated header, added libraries

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+/* Libraries */
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,23 +16,27 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
+
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
 
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
 
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
 
-
-void binary_tree_print(const binary_tree_t *);
+/* Prototypes */
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -47,8 +56,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-
 
 
 #endif /* _BINARY_TREES_H_ */
